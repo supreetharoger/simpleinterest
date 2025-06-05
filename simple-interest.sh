f4ae3fccb2d10a62b68c53f6a54e5e65362185e3
Supreetha Almeida <iris.0287@gmail.com>
2025-06-05 01:59:09 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+echo Enter principle amount
+read amount #principle amount taken as amount
+echo enter rate of interest
+read rate #rate of interest taken as rate
+echo enter time period in error
+read t #time period is taken as tinterest=$(echo "($amount*$rate*$t)/100" | bc -l ) #simple interest formula 
+echo simple interest : $interest
